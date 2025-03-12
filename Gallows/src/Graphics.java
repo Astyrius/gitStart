a28aa6fba6a90dda6ef2d52965f89f8c24982b2e
Astirius <146459919+Astyrius@users.noreply.github.com>
2025-03-12 22:53:38 +0300

    Add files via upload

---
+++ Gallows/src/Graphics.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Graphics {
+    public void StringOfWord(char[] CharWords){
+
+        for (char ww : CharWords) {
+            System.out.print(ww);
+        }
+        System.out.println();
+    }
+
+
+
+
+
+}
---
+++ Gallows/src/Test.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.logging.Handler;
+
+public class Test {
+    public static void main(String[] args) {
+        String m  = "gwf";
+        int d = 546;
+        int n;
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a % 2 == 0 && b % 2 == 0){
+            System.out.println("Удачное совпадение");
+        }
+
+        int day = 15; // Пример числа
+        String decade;
+
+        if (day >= 1 && day <= 10) {
+            decade = "первая декада";
+        } else if (day >= 11 && day <= 20) {
+            decade = "вторая декада";
+        } else if (day >= 21 && day <= 31) {
+            decade = "третья декада";
+        }
+
+         */
+
+    }
+}
+
+
